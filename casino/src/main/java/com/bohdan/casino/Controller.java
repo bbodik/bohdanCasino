977ed8ffb9bfa698a12edb2cbb022106384b9c24
bohdan <bbodikk06@gmail.com>
2023-10-17 20:22:48 +0300

    added classic button <3

---
+++ casino/src/main/java/com/bohdan/casino/Controller.java
@@ -0,0 +1,15 @@
+package com.bohdan.casino;
+
+
+import com.almasb.fxgl.dsl.FXGL;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+public class Controller {
+@FXML
+private Pane mainMenuPane;
+    @FXML
+    protected void startClassicMode(){
+        mainMenuPane.setVisible(false);
+    }
+}
