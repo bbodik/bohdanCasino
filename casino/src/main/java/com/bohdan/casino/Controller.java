1f00c4577fd52c7e6df868353ac819d7b7bc69bd
bohdan <bbodikk06@gmail.com>
2023-10-18 10:55:19 +0300

    new method for adding icons for game

--- casino/src/main/java/com/bohdan/casino/Controller.java
+++ casino/src/main/java/com/bohdan/casino/Controller.java
@@ -1,11 +1,24 @@
 package com.bohdan.casino;
 
-
 import com.almasb.fxgl.dsl.FXGL;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
 public class Controller {
+
+    public Image[] takeIcon(){
+        String[] fileNames = {"lemon.png", "cherry.png", "grape.png", "bar.png", "coin.png", "seven.png"};
+
+        Image[] images = new Image[6];
+        for (int i = 0; i < fileNames.length; i++) {
+            images[i] = new Image(getClass().getResourceAsStream("/" + fileNames[i]));
+        }
+        return images;
+    }
 @FXML
 private Pane mainMenuPane;
     @FXML
