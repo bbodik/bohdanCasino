89253a9667eb9b59406f7fe344516726bf191de4
bohdan <bbodikk06@gmail.com>
2023-10-18 11:15:49 +0300

    uppdeted icons

--- casino/src/main/java/com/bohdan/casino/Controller.java
+++ casino/src/main/java/com/bohdan/casino/Controller.java
@@ -4,11 +4,16 @@ import com.almasb.fxgl.dsl.FXGL;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
 public class Controller {
+    @FXML
+    private Pane mainMenuPane;
+    @FXML
+    private ImageView icon1,icon2,icon3,icon4,icon5;
 
     public Image[] takeIcon(){
         String[] fileNames = {"lemon.png", "cherry.png", "grape.png", "bar.png", "coin.png", "seven.png"};
@@ -19,8 +24,7 @@ public class Controller {
         }
         return images;
     }
-@FXML
-private Pane mainMenuPane;
+
     @FXML
     protected void startClassicMode(){
         mainMenuPane.setVisible(false);
@@ -30,5 +34,10 @@ private Pane mainMenuPane;
     protected void startCrashMode(){
         mainMenuPane.setVisible(false);
 
+    }
+    @FXML
+    protected void spin(){
+        Image[] icons = takeIcon();
+        icon1.setImage(icons[1]);
     }
 }
