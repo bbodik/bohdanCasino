ee3744bde22e72cb16ad68eb07fac6e039174795
bohdan <bbodikk06@gmail.com>
2023-10-18 17:38:45 +0300

    fixed 1/2 of them

--- casino/src/main/java/com/bohdan/casino/Controller.java
+++ casino/src/main/java/com/bohdan/casino/Controller.java
@@ -2,6 +2,7 @@ package com.bohdan.casino;
 
 import com.almasb.fxgl.dsl.FXGL;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -21,21 +22,29 @@ public class Controller {
             new Image(getClass().getResourceAsStream("/coin.png")),
             new Image(getClass().getResourceAsStream("/seven.png"))
     };
+    private int counter;
     @FXML
     private Pane mainMenuPane, classicGamePane;
     @FXML
-    private ImageView icon1, icon2, icon3, icon4, icon5;
+    private Button spinButton;
+    @FXML
+    private ImageView icon1, icon2, icon3, icon4;
 
+    private ImageView[] iconsArr = {icon1, icon2, icon3, icon4,};
 
     @FXML
     protected void startClassicMode() {
         mainMenuPane.setVisible(false);
         classicGamePane.setVisible(true);
+        iconsArr[0] = icon1;
+        iconsArr[1] = icon2;
+        iconsArr[2] = icon3;
+        iconsArr[3] = icon4;
         icon1.setImage(icons[5]);
         icon2.setImage(icons[5]);
         icon3.setImage(icons[5]);
         icon4.setImage(icons[5]);
-        icon5.setImage(icons[5]);
+
     }
 
     @FXML
@@ -45,25 +54,23 @@ public class Controller {
 
     @FXML
     protected void spin() {
-        Thread[] threads = new Thread[5];
-        threads[0] = createIconThread(icon1, icons);
-        threads[1] = createIconThread(icon2, icons);
-        threads[2] = createIconThread(icon3, icons);
-        threads[3] = createIconThread(icon4, icons);
-        threads[4] = createIconThread(icon5, icons);
+        counter = iconsArr.length;
+        spinButton.setDisable(true);
+        Thread[] threads = new Thread[iconsArr.length];
+
+
+        Object lock = new Object();
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = createIconThread(iconsArr[i], icons, lock);
+        }
         for (Thread thread : threads) {
             thread.start();
         }
-        for (Thread thread : threads) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                System.out.println("Thread interrupted");
-            }
-        }
+
     }
 
-    private Thread createIconThread(ImageView icon, Image[] icons) {
+    private Thread createIconThread(ImageView icon, Image[] icons, Object lock) {
         return new Thread(() -> {
             Random rnd = new Random();
             int randomTime = rnd.nextInt(15, 20);
@@ -76,7 +83,14 @@ public class Controller {
                     System.out.println("not sleep");
                 }
             }
+            synchronized (lock) {
+                counter--;
+                if (counter == 0) {
+                    spinButton.setDisable(false);
+                }
+            }
         });
     }
+
 }
 
