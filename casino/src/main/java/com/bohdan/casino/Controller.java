f47788e5d4a2b7f8912b63ccb2d4825c6f455428
bohdan <bbodikk06@gmail.com>
2023-10-18 16:44:04 +0300

    got new bugs, tried

--- casino/src/main/java/com/bohdan/casino/Controller.java
+++ casino/src/main/java/com/bohdan/casino/Controller.java
@@ -6,38 +6,77 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+
 import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
+
 public class Controller {
+    Image[] icons = {
+            new Image(getClass().getResourceAsStream("/lemon.png")),
+            new Image(getClass().getResourceAsStream("/cherry.png")),
+            new Image(getClass().getResourceAsStream("/grape.png")),
+            new Image(getClass().getResourceAsStream("/bar.png")),
+            new Image(getClass().getResourceAsStream("/coin.png")),
+            new Image(getClass().getResourceAsStream("/seven.png"))
+    };
     @FXML
-    private Pane mainMenuPane;
+    private Pane mainMenuPane, classicGamePane;
     @FXML
-    private ImageView icon1,icon2,icon3,icon4,icon5;
+    private ImageView icon1, icon2, icon3, icon4, icon5;
 
-    public Image[] takeIcon(){
-        String[] fileNames = {"lemon.png", "cherry.png", "grape.png", "bar.png", "coin.png", "seven.png"};
 
-        Image[] images = new Image[6];
-        for (int i = 0; i < fileNames.length; i++) {
-            images[i] = new Image(getClass().getResourceAsStream("/" + fileNames[i]));
+    @FXML
+    protected void startClassicMode() {
+        mainMenuPane.setVisible(false);
+        classicGamePane.setVisible(true);
+        icon1.setImage(icons[5]);
+        icon2.setImage(icons[5]);
+        icon3.setImage(icons[5]);
+        icon4.setImage(icons[5]);
+        icon5.setImage(icons[5]);
+    }
+
+    @FXML
+    protected void startCrashMode() {
+        mainMenuPane.setVisible(false);
+    }
+
+    @FXML
+    protected void spin() {
+        Thread[] threads = new Thread[5];
+        threads[0] = createIconThread(icon1, icons);
+        threads[1] = createIconThread(icon2, icons);
+        threads[2] = createIconThread(icon3, icons);
+        threads[3] = createIconThread(icon4, icons);
+        threads[4] = createIconThread(icon5, icons);
+        for (Thread thread : threads) {
+            thread.start();
         }
-        return images;
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted");
+            }
+        }
     }
 
-    @FXML
-    protected void startClassicMode(){
-        mainMenuPane.setVisible(false);
-
-    }
-    @FXML
-    protected void startCrashMode(){
-        mainMenuPane.setVisible(false);
-
-    }
-    @FXML
-    protected void spin(){
-        Image[] icons = takeIcon();
-        icon1.setImage(icons[1]);
+    private Thread createIconThread(ImageView icon, Image[] icons) {
+        return new Thread(() -> {
+            Random rnd = new Random();
+            int randomTime = rnd.nextInt(15, 20);
+            for (int i = 0; i < randomTime; i++) {
+                int randomIcon = rnd.nextInt(6);
+                icon.setImage(icons[randomIcon]);
+                try {
+                    Thread.sleep(1200 - i * 59);
+                } catch (InterruptedException e) {
+                    System.out.println("not sleep");
+                }
+            }
+        });
     }
 }
+
